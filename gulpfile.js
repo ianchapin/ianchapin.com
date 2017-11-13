7adad635d609bb3d859c55470e4d2d9431f26fb8
Ian Chapin <ianchapin@gmail.com>
2017-11-13 02:53:57 -0600

    Initiated project for ianchapin.com

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+
+/*
+    -- TOP LEVEL FUNCTIONS --
+    gulp.task - Define tasks
+    gulp.src - Point to files to use
+    gulp.dest - Points to folder to output
+    gulp.watch - Watch files and folders for changes  
+*/
+
+
+gulp.task('default', function () {
+    return console.log('gulp is running...');
+});

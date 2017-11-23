a05771a2ad94cfa22192317995d6d6e1dd53adf6
Ian Chapin <ianchapin@gmail.com>
2017-11-23 01:22:37 -0600

    fixed banner gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,9 @@ var gulp = require('gulp'),
 
 // Set the banner content
 var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Ian Chapin Web Design - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
   ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/ianchapin/<%= pkg.name %>/blob/master/LICENSE)\n',
   ' */\n',
   ''
 ].join('');
